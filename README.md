6c4cde1b97337eed2dc211ecb4ad131e90f82543
Cees Kettenis <ckettenis@gmail.com>
2022-03-27 13:37:09 +0800

    Added AI
    -- Added an AI with 10 difficulty levels
    -- cleaned up the code
    -- made the game continually play until a manual reset is pressed.
    -- made a simple start screen with the controls.
    -- updated exe

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-Hello my name is Cees!
+Hello my name is Cees
 
 This is my first ever game made with python and pygame.
 
-I found this amazing youtube tutorial that i followed along with and improved the code with some extra features.
+I found this amazing youtube tutorial that i followed along with and improved the code with some extra features of my own.
 Check out Coding Spot if you wish to learn yourself: https://www.youtube.com/channel/UCLqXQLK6zKZg0trhanjAkkQ
 
 Feel free to copy and paste my code and use it in anyway you like.
--- start.py
+++ start.py
@@ -1,22 +1,23 @@
-import sys, pygame, random, time,math
-from turtle import width
+import sys, pygame, random, time
 from pygame.locals import *
 
 pygame.init()
-pygame.mouse.set_visible(0)
 
 #----------
 # CONSTANTS
 #----------
-
-infoobject = pygame.display.Info()
-WIDTH = infoobject.current_w
-HEIGHT = infoobject.current_h
-#WIDTH = 800
-#HEIGHT = 500
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+infoobject = pygame.display.Info()
+
+#Release Mode
+WIDTH = infoobject.current_w
+HEIGHT = infoobject.current_h
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+
+#Debug Mode
+#WIDTH = 800
+#HEIGHT = 500
 #screen = pygame.display.set_mode((WIDTH,HEIGHT))
 
 pygame.display.set_caption('My Pong')
@@ -41,12 +42,16 @@ class Ball:
 		pygame.draw.circle(self.screen, self.color, (int(self.posX), int(self.posY)), int(self.radius))
 
 	def start_moving(self):
+		#pick a random direction
 		number = round(random.uniform(0, 1))
 		if number == 0:
 			number = -1
 
+		#pick an random speed to start the ball off width
 		randomdx = random.randint(300,450)
 		randomdy = random.randint(100, 250)
+
+		#set the direction with the speed.
 		self.dx = randomdx * number
 		self.dy =  randomdy * number
 	
@@ -56,14 +61,20 @@ class Ball:
 
 
 	def paddle_collision(self):
+		#reverse the direction when it hits a paddle
 		self.dx = self.dx * -1
+
+		#increase its speed over time by a small amount to make the game harder and harder.
 		if self.dx < 0:
 			self.dx = self.dx - 25
 		elif self.dx > 0:
 			self.dx = self.dx + 25
 
 	def wall_collision(self):
+		#reverse the direction when it hits a wall.
 		self.dy = self.dy * -1
+
+		#increase its speed over time by a small amount to make the game harder and harder.
 		if self.dy < 0:
 			self.dy = self.dy - 25 
 		elif self.dy > 0:
@@ -72,8 +83,7 @@ class Ball:
 	def restart_pos(self):
 		self.posX = WIDTH//2
 		self.posY = HEIGHT//2
-		self.dx = 0
-		self.dy = 0
+		self.start_moving()
 		self.show()
 
 class Paddle:
@@ -90,11 +100,25 @@ class Paddle:
 	def show(self):
 		pygame.draw.rect( self.screen, self.color,(self.posX, self.posY, self.width, self.heigh))
 	
+	#player control.
 	def move(self, dt):
 		if self.state == 'up':
 			self.posY -= 500 * dt
 		elif self.state == 'down':
 			self.posY += 500 * dt
+
+	#simple AI that moves up and down to align itself with the balls posY
+	def self_move(self, dt, ball):
+		if self.posY + self.heigh//2 > ball.posY:
+			self.state = 'up'
+			self.posY -= AISPEED * dt			
+		elif self.posY + self.heigh//2 < ball.posY:
+			self.state = 'down'
+			self.posY += AISPEED * dt
+		
+		self.state = 'stopped'
+		self.posY = self.posY
+
 
 	def clamp(self):
 		if self.posY <= 0:
@@ -168,12 +192,84 @@ class CollisionManager:
 # FUNCTIONS
 #----------
 
+# VARIABLES
+PLAYERS = 1
+AISPEED = 400 #normal
+playing = False
+dt = 0
+prev_time = time.time()
+
+def get_AI_Mode():
+	AIMODE = "Normal"
+	if AISPEED == 100:
+		AIMODE = "Super Baby"
+	elif AISPEED == 200:
+		AIMODE = "Baby"
+	elif AISPEED == 300:
+		AIMODE = "Easy"
+	elif AISPEED == 400:
+		AIMODE = "Normal"
+	elif AISPEED == 500:
+		AIMODE = "Human"
+	elif AISPEED == 600:
+		AIMODE = "A Challange"
+	elif AISPEED == 700:
+		AIMODE = "Super Human"
+	elif AISPEED == 800:
+		AIMODE = "Pong Genius"
+	elif AISPEED == 900:
+		AIMODE = "Are you sure?"
+	elif AISPEED == 1000:
+		AIMODE = "God Mode!"
+	
+	return AIMODE
+
+# a function to draw the line and background. and if needed the controls.
 def paint_black():
 	screen.fill(BLACK)
-	pygame.draw.line( screen, WHITE, (WIDTH//2, 0), (WIDTH//2, HEIGHT), 5 )
 
-paint_black()
+	#show the controls when we are not playing.
+	if playing == False:
+		AI_Mode_Text = get_AI_Mode()
 
+		#i could figure out how to do this in a for or foreach loop. but from a few google searches this seemed quite hard in pygame.
+		font = pygame.font.SysFont("monospace", int(40*WIDTH / WIDTH), bold=True)
+		label = font.render("CONTROLS", 0, WHITE)
+		font2 = pygame.font.SysFont("monospace", int(20*WIDTH / WIDTH), bold=False)
+		label2 = font2.render("1 = 1 player game.", 0, WHITE)
+		label3 = font2.render("2 = 2 player game.", 0, WHITE)
+		label4 = font2.render("3 = decrease AI difficulty.", 0, WHITE)
+		label5 = font2.render("4 = increase AI difficulty.", 0, WHITE)
+		label6 = font2.render("p = start game.", 0, WHITE)
+		label7 = font2.render("r = reset game.", 0, WHITE)
+		label8 = font2.render("Space = Close Game.", 0, WHITE)
+
+		label9 = font2.render(("AI Difficulty: {}").format(AI_Mode_Text), 0, WHITE)
+
+		widthOffSet = WIDTH//2 - label.get_rect().width
+		screen.blit(label, (widthOffSet, HEIGHT//5))
+		increasedHeight = label.get_rect().height
+		screen.blit(label2, (widthOffSet, (HEIGHT//5 + increasedHeight)))
+		increasedHeight += label2.get_rect().height
+		screen.blit(label3, (widthOffSet, (HEIGHT//5 + increasedHeight)))
+		increasedHeight += label3.get_rect().height
+		screen.blit(label4, (widthOffSet, (HEIGHT//5 + increasedHeight)))
+		increasedHeight += label4.get_rect().height		
+		screen.blit(label5, (widthOffSet, (HEIGHT//5 + increasedHeight)))
+		increasedHeight += label5.get_rect().height	
+		screen.blit(label6, (widthOffSet, (HEIGHT//5 + increasedHeight)))
+		increasedHeight += label6.get_rect().height
+		screen.blit(label7, (widthOffSet, (HEIGHT//5 + increasedHeight)))
+		increasedHeight += label7.get_rect().height
+		screen.blit(label8, (widthOffSet, (HEIGHT//5 + increasedHeight)))
+		increasedHeight += label8.get_rect().height*2
+		
+		if PLAYERS == 1:
+			screen.blit(label9, (widthOffSet, (HEIGHT//5 + increasedHeight)))
+	elif playing == True:
+		pygame.draw.line( screen, WHITE, (WIDTH//2, 0), (WIDTH//2, HEIGHT), 5 )
+
+#to restart the game.
 def restart():
 	paint_black()
 	scoreLeft.restart()
@@ -181,8 +277,10 @@ def restart():
 	paddleLeft.restart_pos()
 	paddleRight.restart_pos()
 	ball.restart_pos()
+	pygame.mouse.set_visible(1)
 
-#objects
+
+#Create Objects for the game.
 ball = Ball(screen, WHITE, WIDTH//2, HEIGHT//2, 15)
 paddleLeft = Paddle(screen, WHITE, 15, HEIGHT//2-60, 20, 120)
 paddleRight = Paddle(screen, WHITE, WIDTH - 20 - 15, HEIGHT//2 -60, 20, 120)
@@ -190,23 +288,18 @@ collision = CollisionManager()
 scoreLeft = Score(screen, "0", WIDTH//4, 15)
 scoreRight = Score(screen, "0", WIDTH - WIDTH//4, 15)
 
-# VARIABLES
-playing = False
-dt = 0
-prev_time = time.time()
-
 while 1:
-	#compute delta time.
+	#compute delta time. so the ball doesnt go BRRRRRRR.
 	now = time.time()
 	dt = now - prev_time
 	prev_time = now
 
+	#check for user input.
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 		
 		if event.type == pygame.KEYDOWN:
-			#the start button.
 			if event.key == pygame.K_p:
 				ball.start_moving()
 				playing = True
@@ -214,41 +307,75 @@ while 1:
 			if event.key == pygame.K_SPACE:
 				sys.exit()
 
+			if event.key == pygame.K_1 or event.key == pygame.K_KP1:
+				if playing == False:
+					PLAYERS = 1
+
+			if event.key == pygame.K_2 or event.key == pygame.K_KP2:
+				if playing == False:
+					PLAYERS = 2
+
+			if event.key == pygame.K_4 or event.key == pygame.K_KP4:
+				if playing == False:
+					AISPEED += 100
+					if AISPEED > 1000:
+						AISPEED = 1000
+
+			if event.key == pygame.K_3 or event.key == pygame.K_KP3:
+				if playing == False:
+					AISPEED -= 100
+					if AISPEED < 100:
+						AISPEED = 100
+
 			#the restart button
 			if event.key == pygame.K_r:
 				restart()
 				playing = False
+				
 
 			#if the key is pressed move the paddles.
 			if event.key == pygame.K_w:
 				paddleLeft.state = 'up'
 			if event.key == pygame.K_s:
 				paddleLeft.state = 'down'
-				
-			if event.key == pygame.K_UP:
-				paddleRight.state = 'up'
-			if event.key == pygame.K_DOWN:
-				paddleRight.state = 'down'
+
+			if PLAYERS == 2:	
+				if event.key == pygame.K_UP:
+					paddleRight.state = 'up'
+				if event.key == pygame.K_DOWN:
+					paddleRight.state = 'down'
 		
 		#check if the player lifted the key so we can stop the paddle.
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_w or event.key == pygame.K_s:
 				paddleLeft.state = 'stopped'
-			if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-				paddleRight.state = 'stopped'
-
+			if PLAYERS == 2:
+				if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+					paddleRight.state = 'stopped'
+	
+	paint_black()
+	
 	if playing:
-		paint_black()
+		pygame.mouse.set_visible(0)
+        #the scores
+		scoreLeft.show()
+		scoreRight.show()
 
 		#ball movement
 		ball.move(dt)
 		ball.show()
 
+		#move player 1
 		paddleLeft.move(dt)
 		paddleLeft.clamp()
 		paddleLeft.show()
 
-		paddleRight.move(dt)
+		#move player 2 or AI
+		if PLAYERS == 2:
+			paddleRight.move(dt)
+		elif PLAYERS != 2:
+			paddleRight.self_move(dt, ball)
+
 		paddleRight.clamp()
 		paddleRight.show()
 
@@ -263,19 +390,16 @@ while 1:
 		if collision.check_goal_left(ball):
 			paint_black()
 			scoreLeft.increase()
-			ball.restart_pos()
 			paddleLeft.restart_pos()
 			paddleRight.restart_pos()
-			playing = False
+			ball.restart_pos()
+
 		if collision.check_goal_right(ball):
 			paint_black()
 			scoreRight.increase()
-			ball.restart_pos()
 			paddleLeft.restart_pos()
 			paddleRight.restart_pos()
-			playing = False
+			ball.restart_pos()
 
-	scoreLeft.show()
-	scoreRight.show()
 	
 	pygame.display.update()
